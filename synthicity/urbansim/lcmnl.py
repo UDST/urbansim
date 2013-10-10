c933208eacdabfd454d3528d531e02fee3e765a6
Fletcher Foti <ffoti@berkeley.edu>
2013-10-10 14:38:52 -0700

    small changes to clean up output

--- synthicity/urbansim/lcmnl.py
+++ synthicity/urbansim/lcmnl.py
@@ -2,51 +2,58 @@ from synthicity.urbansim import interaction, mnl
 import numpy as np, pandas as pd
 
 GPU = 0
+if GPU: interaction.enable_gpu()
 NUMCLASSES = 2
-MAXITER = 3
+MAXITER = 10000
 NUMALTS = 50
-NUMCHOICES = 100
+NUMCHOICES = 5
 NUMVARS = 10 # placeholder for num variables for both CM and CS models
 data = np.random.random((NUMCHOICES*NUMALTS,NUMVARS))
-beta = [np.zeros(NUMVARS) for i in range(NUMCLASSES)]
-cmbeta = np.zeros(NUMVARS)
+beta = [np.ones(NUMVARS) for i in range(NUMCLASSES)]
+cmbeta = np.ones(NUMVARS)
 cmdata = np.random.random((NUMCHOICES*NUMCLASSES,NUMVARS))
+for i in range(NUMCHOICES): cmdata[i*NUMCLASSES,:] = 0
 chosen = np.zeros((NUMCHOICES,NUMALTS))
 chosen[np.arange(NUMCHOICES),(np.random.random(NUMCHOICES)*NUMALTS).astype('int')] = 1
+
+loglik = -999999
+emtol = 1e-03
 
 for i in range(MAXITER):
   print "Running iteration %d" % (i+1)
 
   # EXPECTATION
   print "Running class membership model"
-  print cmbeta
+  print "cmbeta", cmbeta
   cmprobs = mnl.mnl_simulate(cmdata,cmbeta,NUMCLASSES,GPU=GPU,returnprobs=1)
+  print "cmprobs", cmprobs
 
   csprobs = []
   for cno in range(NUMCLASSES):
     print "Running class specific model for class %d" % (cno+1)
-    print beta[cno]
+    print "csbeta", beta[cno]
     tmp = mnl.mnl_simulate(data,beta[cno],NUMALTS,GPU=GPU,returnprobs=1)
-    print tmp
+    print "csprobs", tmp
     tmp = np.sum(tmp*chosen,axis=1) # keep only chosen probs
     csprobs.append(np.reshape(tmp,(-1,1)))
   csprobs = np.concatenate(csprobs,axis=1)
 
   h = csprobs * cmprobs
+  oldloglik = loglik
   loglik = np.sum(np.log(np.sum(h,axis=1)))
-  print loglik
-  print beta
+  print "current loglik", loglik, i+1
+  print "current beta", beta
+  if abs(loglik-oldloglik) < emtol: break
   wts = h / np.reshape(np.sum(h,axis=1),(-1,1))
-  
+  for i in range(wts.shape[1]): print i, pd.Series(wts[:,i]).describe()
+   
   # MAXIMIZATION
 
   print "Estimating class membership model"
   fit, results = mnl.mnl_estimate(cmdata,None,NUMCLASSES,GPU=GPU,weights=wts,lcgrad=True)
   cmbeta = zip(*results)[0]
-  print cmbeta
 
   for cno in range(NUMCLASSES):
     print "Estimating class specific model for class %d" % (cno+1)
-    print pd.Series(wts[:,cno]).describe()
     fit, results  = mnl.mnl_estimate(data,chosen,NUMALTS,GPU=GPU,weights=wts[:,cno])
     beta[cno] = zip(*results)[0]
