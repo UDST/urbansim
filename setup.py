b05e6cc74f0e0db3b7fd874e90276669751faa0a
Matt Davis <jiffyclub@gmail.com>
2014-03-07 12:16:38 -0800

    Add installation requirements to setup.py.

--- setup.py
+++ setup.py
@@ -13,10 +13,21 @@ setup(
     url='https://github.com/synthicity/urbansim',
     classifiers=['Development Status :: 4 - Beta',
                  'Programming Language :: Python :: 2.7',
-                 'License :: OSI Approved :: GNU Affero General Public License v3'],
+                 ('License :: OSI Approved :: '
+                  'GNU Affero General Public License v3')],
     packages=['synthicity'],
     package_data={'': ['*.py',
                        'urbansim/*.py',
                        'urbansimd/*.py',
-                       'utils/*.py']}
+                       'utils/*.py']},
+    install_requires=['Django>=1.6.2',
+                      'jinja2>=2.7.2',
+                      'numpy>=1.8.0',
+                      'pandas>=0.13.1',
+                      'patsy>=0.2.1',
+                      'pytables>=3.1.0',
+                      'scipy>=0.13.3',
+                      'shapely>=1.3.0',
+                      'simplejson>=3.3.3',
+                      'statsmodels>=0.5.0']
 )
