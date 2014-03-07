f3fc76aa4136ae1d9fafd7f29de383907a56d7ca
Matt Davis <jiffyclub@gmail.com>
2014-03-07 15:49:17 -0800

    PEP8 formatting for sandbox files.

--- sandbox/init.py
+++ sandbox/init.py
@@ -1,8 +1,19 @@
-import os, json, sys, time, pandas as pd, numpy as np
+import json
+import os
+import sys
+import time
+
+import numpy as np
+import pandas as pd
+
 from synthicity.urbansimd import urbansimd
 from synthicity.utils import misc
 from synthicity.utils.misc import run_model
-pd.set_option('precision',3)
-pd.set_option('display.width',160)
-try: import couchdb
-except: pass
+
+pd.set_option('precision', 3)
+pd.set_option('display.width', 160)
+
+try:
+    import couchdb
+except:
+    pass
