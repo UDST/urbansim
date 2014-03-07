2cfd1c7e1174c2aa7208c076f4cd4af7dd5ea397
Matt Davis <jiffyclub@gmail.com>
2014-03-07 15:53:15 -0800

    PEP8 formatting for script files.

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,10 +1,16 @@
-import os, string, sys, json
+import json
+import os
+import string
+import sys
+
 from synthicity.utils import misc
 
 args = sys.argv[1:]
 
 for arg in args:
-  print "Generating %s" % arg
-  basename, d = misc.gen_model(arg)
-  for mode, code in d.items():
-    open(os.path.join(misc.models_dir(),basename+'_'+mode+'.py'),'w').write(code)
+    print "Generating %s" % (arg,)
+    basename, d = misc.gen_model(arg)
+    for mode, code in d.items():
+        open(
+            os.path.join(misc.models_dir(), basename + '_' + mode + '.py'), 'w'
+        ).write(code)
--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,11 @@
-import os, string, sys, simplejson
+import os
+import string
+import sys
+
+import simplejson
+
 from synthicity.utils import misc
-sys.path.insert(0,".")
+sys.path.insert(0, ".")
 import dataset
 
 args = sys.argv[1:]
@@ -8,9 +13,11 @@ dset = dataset.LocalDataset(args[0])
 args = args[1:]
 
 for arg in args:
-  print "Generating %s" % arg
-  item = string.split(arg,',')
-  if len(item) == 1: model, mode = item[0], "run"
-  else: model, mode = item
-  d = misc.run_model(model,dset,mode)
-  #print d
+    print "Generating %s" % arg
+    item = string.split(arg, ',')
+    if len(item) == 1:
+        model, mode = item[0], "run"
+    else:
+        model, mode = item
+    d = misc.run_model(model, dset, mode)
+    # print d
