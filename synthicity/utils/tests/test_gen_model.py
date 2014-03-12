b70d7d608c4710f3b1dec5461fd2f81bf5ed8ab6
Matt Davis <jiffyclub@gmail.com>
2014-03-12 10:53:18 -0700

    Test for locationchoicemodel template.

--- synthicity/utils/tests/test_gen_model.py
+++ synthicity/utils/tests/test_gen_model.py
@@ -9,29 +9,63 @@ from ..misc import gen_model
 
 def test_hedonicmodel():
     config = {
-        'model': 'hedonicmodel',
-        'name': 'unit_test',
-        'show': True,
-        'internalname': 'test_name',
-        'table': 'dset.test_table',
-        "table_sim": "dset.test_sim(thing=False)",
-        'merge': {
-            'right_index': True,
-            'left_on': '_node_id',
-            'table': 'dset.merge'
-        },
-        'segment': ['test_segment'],
-        'dep_var': 'test_dep_var',
-        'dep_var_transform': 'np.log',
-        'ind_vars': [
-            'var1',
-            'var2',
-            'var3'
-        ],
         'add_constant': True,
+        'dep_var': 'averageweightedrent',
+        'dep_var_transform': 'np.log',
+        'ind_vars': ['accessibility', 'reliability', 'ln_stories'],
+        'internalname': 'jobs',
+        'merge': {
+            'left_on': '_node_id',
+            'right_index': True,
+            'table': 'dset.nodes'
+        },
+        'model': 'hedonicmodel',
+        'output_table': 'dset.buildings',
         'output_transform': 'np.exp',
-        'output_varname': 'test_output_var',
-        'output_table': 'dset.output_table'
+        'output_varname': 'nonresidential_rent',
+        'segment': ['general_type'],
+        'table': 'dset.costar',
+        'table_sim': 'dset.building_filter(residential=0)',
+    }
+
+    basename, d = gen_model(config)
+
+    assert basename == 'autorun'
+    assert 'estimate' in d
+    assert 'simulate' in d
+
+    assert 'def autorun_estimate' in d['estimate']
+    assert 'def autorun_simulate' in d['simulate']
+
+    # make sure no errors happen when compiling the code
+    compile(d['estimate'], '<string>', mode='exec')
+    compile(d['simulate'], '<string>', mode='exec')
+
+
+def test_location_choice_model():
+    config = {
+        'alternatives': (
+            'dset.nodes.join('
+            'dset.variables.compute_nonres_building_proportions(dset, year))'),
+        'dep_var': '_node_id',
+        'est_sample_size': 10000,
+        'ind_vars': [
+            'total sqft',
+            'ln_weighted_rent',
+            'retpct',
+            'indpct',
+            'accessibility',
+            'reliability'
+        ],
+        'internalname': 'jobs',
+        'model': 'locationchoicemodel',
+        'modeldependencies': 'nrh.json',
+        'output_table': 'dset.nets',
+        'output_varname': 'firms_building_ids',
+        'sample_size': 100,
+        'segment': ['naics11cat'],
+        'table': 'dset.nets',
+        'table_sim': 'dset.nets[dset.nets.lastmove>2007]',
     }
 
     basename, d = gen_model(config)
