89f97ca2b13f630b8b49a3fd8d7cca2f29095476
Matt Davis <jiffyclub@gmail.com>
2014-04-24 13:46:02 -0700

    Add a RelocationModel class to help with repeated relocations using the same table of rates.

--- urbansim/models/relocation.py
+++ urbansim/models/relocation.py
@@ -96,3 +96,57 @@ def find_movers(choosers, rates):
     movers = relocation_rates.index[
         relocation_rates > np.random.random(len(choosers))]
     return movers
+
+
+class RelocationModel(object):
+    """
+    Find movers within a population according to a table of
+    relocation rates.
+
+    Parameters
+    ----------
+    rates : pandas.DataFrame
+        Table of relocation rates. Index is unused. Must have a
+        'probability_of_relocating' column with fraction relocation
+        rates.
+
+        Other columns describe filters on the `choosers`
+        table so that different segments can have different relocation
+        rates. Columns that ends with '_max' will be used to create
+        a "less than" filters, columns that end with '_min' will be
+        used to create "greater than or equal to" filters.
+        A column with no suffix will be used to make an 'equal to' filter.
+
+        An example `rates` structure:
+
+        age_of_head_max  age_of_head_min
+                    nan               65
+                     65               40
+
+        In this example the `choosers` table would need to have an
+        'age_of_head' column on which to filter.
+
+        nan should be used to flag filters that do not apply
+        in a given row.
+
+    """
+    def __init__(self, rates):
+        self.relocation_rates = rates
+
+    def find_movers(self, choosers):
+        """
+        Select movers from among a table of `choosers` according to the
+        stored relocation rates.
+
+        Parameters
+        ----------
+        choosers : pandas.DataFrame
+            Table of agents from which to find movers.
+
+        Returns
+        -------
+        movers : pandas.Index
+            Suitable for indexing `choosers` by index.
+
+        """
+        return find_movers(choosers, self.relocation_rates)
--- urbansim/models/tests/test_relocation.py
+++ urbansim/models/tests/test_relocation.py
@@ -6,6 +6,26 @@ import pytest
 from .. import relocation as relo
 
 
+@pytest.fixture
+def choosers():
+    return pd.DataFrame(
+        {'var1': range(5),
+         'var2': range(5, 10),
+         'var3': ['q', 'w', 'e', 'r', 't']},
+        index=['a', 'b', 'c', 'd', 'e'])
+
+
+@pytest.fixture
+def rates():
+    return pd.DataFrame(
+        {'var1_min': [np.nan, np.nan, np.nan],
+         'var1_max': [1, np.nan, np.nan],
+         'var2_min': [np.nan, 7, np.nan],
+         'var2_max': [np.nan, 8, np.nan],
+         'var3': [np.nan, np.nan, 't'],
+         'probability_of_relocating': [1, 1, 1]})
+
+
 @pytest.mark.parametrize('name, val, filter_exp', [
     ('x', 1, 'x == 1'),
     ('x', 'a', "x == 'a'"),
@@ -15,20 +35,12 @@ def test_filterize(name, val, filter_exp):
     assert relo._filterize(name, val) == filter_exp
 
 
-def test_find_movers():
-    choosers = pd.DataFrame(
-        {'var1': range(5),
-         'var2': range(5, 10),
-         'var3': ['q', 'w', 'e', 'r', 't']},
-        index=['a', 'b', 'c', 'd', 'e'])
-    rates = pd.DataFrame(
-        {'var1_min': [np.nan, np.nan, np.nan],
-         'var1_max': [1, np.nan, np.nan],
-         'var2_min': [np.nan, 7, np.nan],
-         'var2_max': [np.nan, 8, np.nan],
-         'var3': [np.nan, np.nan, 't'],
-         'probability_of_relocating': [1, 1, 1]})
-
+def test_find_movers(choosers, rates):
     movers = relo.find_movers(choosers, rates)
+    npt.assert_array_equal(movers, ['a', 'c', 'e'])
 
+
+def test_relocation_model(choosers, rates):
+    rm = relo.RelocationModel(rates)
+    movers = rm.find_movers(choosers)
     npt.assert_array_equal(movers, ['a', 'c', 'e'])
