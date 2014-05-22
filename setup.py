0fafc7a89ac85085bbe07743674bb661dcd8c2d3
Fletcher Foti <fscottfoti@gmail.com>
2014-05-22 11:43:27 -0700

    moving urbansimd to usui
    It was decided that urbansimd should not live in urbansim, but should live with the website repository instead.  It brought in a number of dependencies that don't really apply to the general population of urbansim users.

--- setup.py
+++ setup.py
@@ -17,12 +17,8 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3'
     ],
     packages=find_packages(exclude=['*.tests']),
-    package_data={'urbansim.urbansim': ['templates/*.template']},
     install_requires=[
-        'bottle>=0.12.5',
-        'cliff>=1.5.2',
         'Django>=1.6.2',
-        'jinja2>=2.7.2',
         'numpy>=1.8.0',
         'pandas>=0.13.1',
         'patsy>=0.2.1',
@@ -33,13 +29,5 @@ setup(
         'simplejson>=3.3.3',
         'statsmodels>=0.5.0',
         'tables>=3.1.0'
-    ],
-    entry_points={
-        'console_scripts': [
-            'urbansim = urbansim.cli:main'
-        ],
-        'urbansim.commands': [
-            'serve = urbansim.server.servecli:Serve'
-        ]
-    }
+    ]
 )
