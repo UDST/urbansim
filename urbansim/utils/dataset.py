2e652455efe33ba28978be91eb1c1167e2d26dbf
Fletcher Foti <fscottfoti@gmail.com>
2014-05-26 17:27:14 -0700

    removing functions from dataset that are not used
    Biggest change is to move add_field_from_table to misc and call is "reindex."  The description is length and part of the docstring and will not be repeated here.  At any rate, dataset is becoming more parsionious by the minute and will have more documentation soon, I promise.

--- urbansim/utils/dataset.py
+++ urbansim/utils/dataset.py
@@ -25,47 +25,24 @@ class Dataset(object):
         self.coeffs = pd.DataFrame()  # keep all coefficients in memory
         self.attrs = {}  # keep all computed outputs in memory
         self.savetbls = []  # names of tables to write to output hdf5
-
-    def close(self):
-        self.store.close()
+        self.scenario = "baseline"
 
     def list_tbls(self):
         return list(set([x[1:] for x in self.store.keys()] + self.d.keys()))
 
-    def save_toinputfile(self, name, df):
-        self.store[name] = df
-
     def save_tmptbl(self, name, tbl):
-        # used to pass a different table instead of the one that comes from the
-        # database
         self.d[name] = tbl
 
-    def save_table(self, name):
-        if name not in self.savetbls:
-            self.savetbls.append(name)
-
     def save_output(self, filename):
-        outstore = pd.HDFStore(filename)
-        for key in self.attrs.keys():
-            df = self.attrs[key]
-            df = misc.df64bitto32bit(df)
-            print key + "\n", df.describe()
-            outstore[key] = df
+        outstore = pd.HDFStore(filename, savetbls)
 
-        for key in self.savetbls:
+        for key in savetbls:
             df = self.fetch(key)
             df = misc.df64bitto32bit(df)
             print key + "\n", df.describe()
             outstore[key] = df
 
         outstore.close()
-
-    def fetch_csv(self, name, **kwargs):
-        if name in self.d:
-            return self.d[name]
-        tbl = pd.read_csv(os.path.join(misc.data_dir(), name), **kwargs)
-        self.d[name] = tbl
-        return tbl
 
     def fetch(self, name, **kwargs):
         if name in self.d:
@@ -78,9 +55,6 @@ class Dataset(object):
         else:
             tbl = self.store[name]
 
-        if hasattr(self, "modify_table"):
-            tbl = self.modify_table(name, tbl, **kwargs)
-
         self.d[name] = tbl
 
         return tbl
@@ -91,25 +65,6 @@ class Dataset(object):
         if "fetch_" in name:
             raise Exception()
         return self.fetch(name)
-
-    # this is a shortcut function to join the table with dataset.fetch(tblname)
-    # using the foreign_key in order to add fieldname to the source table
-    def join_for_field(self, table, tblname, foreign_key, fieldname):
-        if isinstance(table, str):
-            table = self.fetch(table)
-        if foreign_key is None:  # join on index
-            # return
-            # pd.merge(table, self.fetch(tblname)[[fieldname]],
-            #          left_index=True, right_index=True)
-            table[fieldname] = self.fetch(
-                tblname)[fieldname].loc[table.index].values
-        else:
-            # return
-            # pd.merge(table, self.fetch(tblname)[[fieldname]],
-            #          left_on=foreign_key, right_index=True)
-            table[fieldname] = self.fetch(
-                tblname)[fieldname].loc[table[foreign_key]].values
-        return table
 
     def compute_range(self, attr, dist, agg=np.sum):
         travel_data = self.fetch('travel_data').reset_index(level=1)
@@ -128,4 +83,17 @@ class Dataset(object):
 
         df = pd.merge(df, self.buildings[cols],
                       left_on='building_id', right_index=True)
+        return df
+
+
+class CustomDataFrame:
+    def __init__(self):
+        pass
+
+    def build_df(obj, flds=None):
+        if flds is None:
+            flds = obj.flds
+        columns = [getattr(obj, fld) for fld in flds]
+        df = pd.concat(columns, axis=1)
+        df.columns = flds
         return df
--- urbansim/utils/misc.py
+++ urbansim/utils/misc.py
@@ -89,6 +89,34 @@ def get_run_number():
     return num
 
 
+def reindex(series1, series2):
+    """
+    This reindexes the first series by the second series.  This is an extremely
+    common operation that does not appear to  be in Pandas at this time.
+    If anyone knows of an easier way to do this in Pandas, please inform the
+    UrbanSim developers.
+
+    The canonical example would be a parcel series which has an index which is
+    parcel_ids and a value which you want to fetch, let's say it's land_area.
+    Another dataset, let's say of buildings has a series which indicate the
+    parcel_ids that the buildings are located on, but which does not have
+    land_area.  If you pass parcels.land_area as the first series and
+    buildings.parcel_id as the second series, this function returns a series
+    which is indexed by buildings and has land_area as values and can be
+    added to the buildings dataset.
+
+    In short, this is a join on to a different table using a foreign key
+    stored in the current table, but with only one attribute rather than
+    for a full dataset.
+
+    This is very similar to the pandas "loc" function or "reindex" function,
+    but neither of those functions return the series indexed on the current
+    table.  In both of those cases, the series would be indexed on the foreign
+    table and would require a second step to change the index.
+    """
+    return pd.Series(series1.loc[series2.values].values, index=series2.index)
+
+
 def signif(val):
     """
     Convert a statistical significance to its ascii representation - this
