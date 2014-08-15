4044e54b6041621e02a743d391b316d80eca7003
scott bridwell <bridwell@gmail.com>
2014-08-14 17:58:51 -0700

    tweak and debug sample_rows method

--- urbansim/utils/sampling.py
+++ urbansim/utils/sampling.py
@@ -1,54 +1,86 @@
+import math
 import numpy as np
 import pandas as pd
 
-def sample_rows(total, sampling_data, with_replace=True, accounting_data=None, foreign_key_column=None, max_iterations=50):
-	"""
-	comments to go here...
+def sample_rows(total, data, replace=True, accounting_column=None, max_iterations=50):
+    """
+    Samples and returns rows from a data frame while matching a desired control total. The total may represent a simple 
+    row count or may attempt to match a sum/quantity from an accounting column. 
 
-    todo: make the accounting stuff either a delegate or a computed column??
-	"""
+    Parameters
+    ----------
+    total : int
+        The control total the sampled rows will attempt to match.
+    data: pandas.DataFrame
+        Table to sample from.
+    replace: bool, optional, default True
+        Indicates if sampling with or without replacement.
+    accounting_column: string, optional
+        Name of column with accounting totals/quanties to apply towards the control. If not provided 
+        then row counts will be used for accounting.
+    max_iterations: int, optional, default 50
+        When using an accounting attribute, the maximum number of sampling iterations 
+        that will be applied. 
 
-	# simplest case, just return n random rows from the sampling frame
-	if accounting_data is None:
-		return sampling_data.loc[np.random.choice(sampling_data.index.values, total, replace=with_replace)].copy()
+    Returns
+    -------
+    sample_rows : pandas.DataFrame
+        Table containing the sample.
+    num_iterations : int
+        Number of iterations it took to hit the control (temporary for testing), only
+        used when accounting_column is provided
+    """
 
-	# determine avg number of accounting records per sample (e.g. persons per household, jobs per business)
-	# todo: throw an error if ther sampling row count is larger than the accounting row count
-	per_sample = len(accounting_data.index) / (1.0 * len(sampling_data.index))
-    
-	# do the intial sample 
-	num_samples = int(math.ceil(math.fabs(total) / per_sample))
-    if with_replace:
-    	sample_idx = sampling_data.index.values
+    # simplest case, just return n random rows
+    if accounting_column is None:
+        if replace == False and total > len(data.index.values):
+            raise ValueError('Control total exceeds the available samples')
+        return data.loc[np.random.choice(data.index.values, total, replace=replace)].copy()
+
+    # make sure this is even feasible
+    if replace == False and total > data[accounting_column].sum():
+        raise ValueError('Control total exceeds the available samples')
+
+    # determine avg number of accounting items per sample (e.g. persons per household, jobs per business)
+    per_sample = data[accounting_column].sum() / (1.0 * len(data.index.values))
+
+    # do the intial sample 
+    num_samples = int(math.ceil(total / per_sample))
+    if replace: 
+        sample_idx = data.index.values
         sample_ids = np.random.choice(sample_idx, num_samples)
     else:
-    	sample_idx = np.random.permutation(sampling_data.index.values)
+        sample_idx = np.random.permutation(data.index.values)
         sample_ids = sample_idx[0:num_samples]
-        samplePos = num_samples
+        sample_pos = num_samples
 
-    sample_rows = sampling_data.loc[sample_ids].copy()
-    curr_total = len(sample_rows.merge(accounting_data, left_index=True, right_on=foreign_key_column).index)
+    sample_rows = data.loc[sample_ids].copy()
+    curr_total = sample_rows[accounting_column].sum()
+    
+    # iteratively refine the sample until we match the accounting total
+    for i in range(0, max_iterations):
 
-	# iteratively refine the sample until we match the accounting total
-	for i in range(0, max_iterations)
-
-		# keep going if we haven't hit the control
+        # keep going if we haven't hit the control
         remaining = total - curr_total
         if remaining == 0: break
         num_samples = int(math.ceil(math.fabs(remaining) / per_sample))
 
         if remaining > 0:
             # we're short, keep sampling 
-            if with_replace:
-            	curr_ids = np.random.choice(sample_idx, num_samples)
+            if replace:
+                curr_ids = np.random.choice(sample_idx, num_samples)
             else:
-            	curr_ids = sample_idx[samplePos:samplePos + num_samples]
-            	samplePos += num_samples
+                curr_ids = sample_idx[sample_pos:sample_pos + num_samples]
+                sample_pos += num_samples
+
+            curr_rows = data.loc[curr_ids].copy()
+            sample_rows = pd.concat([sample_rows, curr_rows])
+            curr_total += curr_rows[accounting_column].sum()
         else:
             # we've overshot, remove from existing samples (FIFO)
-            # if replacement is false do these need to go back into the indexes?
-            curr_rows = sample_rows[:numSamples]
-            sample_rows = sampleRows[numSamples:]
-            currTotal -= len(currRows.merge(child_data, left_index=True, right_on=key_field).index)
-
-    return sample_rows.copy()
+            curr_rows = sample_rows[:num_samples]
+            sample_rows = sample_rows[num_samples:]
+            curr_total -= curr_rows[accounting_column].sum()
+            if not replace: np.append(sample_idx, curr_rows.index.values) 
+    
+    return sample_rows.copy(), i + 1
