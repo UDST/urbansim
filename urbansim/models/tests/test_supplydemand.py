10d8752b212db916115b5a2ecf04a66aa8d62bf1
Matt Davis <jiffyclub@gmail.com>
2014-10-03 16:32:52 -0700

    Update supply_and_demand to work with LCM filters
    When calculating probabilities LCMs filter the set of
    alternatives available to users. In supply_and_demand we need
    to work with the same set of filtered alternatives.

--- urbansim/models/tests/test_supplydemand.py
+++ urbansim/models/tests/test_supplydemand.py
@@ -24,16 +24,21 @@ def alternatives():
         index=pd.Index([x for x in 'abcdefghij'], name='thing_id'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def alt_segmenter():
     return 'zone_id'
 
 
 class _TestLCM(object):
+    def apply_predict_filters(self, choosers, alternatives):
+        choosers = choosers.query('var1 != 7')
+        alternatives = alternatives.query('var2 != 14')
+        return choosers, alternatives
+
     def summed_probabilities(self, choosers, alternatives):
         return [
-            1, 0.25, 1, 2, 1, 0.75, 2, 1, 1.5, 0.5]
-        #   w, x,    y, z, z, x,    y, w, y,   y
+            1, 0.25, 1, 2, 0.75, 2, 1, 1.5, 0.5]
+        #   w, x,    y, z, x,    y, w, y,   y
 
 
 @pytest.fixture(scope='module')
@@ -41,38 +46,51 @@ def lcm():
     return _TestLCM()
 
 
-def test_calculate_adjustment_clips(
-        lcm, choosers, alternatives, alt_segmenter):
+@pytest.fixture
+def filtered(lcm, choosers, alternatives):
+    return lcm.apply_predict_filters(choosers, alternatives)
+
+
+@pytest.fixture(scope='module')
+def wxyz():
+    w = 1
+    x = 0.5
+    y = 1.25
+    z = 2
+    return w, x, y, z
+
+
+def test_calculate_adjustment_clips(lcm, filtered, alt_segmenter):
     clip = 1
+
+    choosers, alternatives = filtered
 
     alts_multiplier, submarkets_multiplier = supdem._calculate_adjustment(
         lcm, choosers, alternatives, alternatives[alt_segmenter], clip, clip)
 
     pdt.assert_series_equal(
-        alts_multiplier, pd.Series([1] * 10, index=alternatives.index),
+        alts_multiplier, pd.Series([1] * 9, index=alternatives.index),
         check_dtype=False)
     pdt.assert_series_equal(
         submarkets_multiplier, pd.Series([1] * 4, index=['w', 'x', 'y', 'z']),
         check_dtype=False)
 
 
-def test_calculate_adjustment(
-        lcm, choosers, alternatives, alt_segmenter):
+def test_calculate_adjustment(lcm, filtered, alt_segmenter, wxyz):
     clip_low = 0
     clip_high = 2
+
+    choosers, alternatives = filtered
 
     alts_multiplier, submarkets_multiplier = supdem._calculate_adjustment(
         lcm, choosers, alternatives, alternatives[alt_segmenter],
         clip_low, clip_high)
 
-    w = 1
-    x = 0.5
-    y = 1.25
-    z = 3 / 2
+    w, x, y, z = wxyz
 
     pdt.assert_series_equal(
         alts_multiplier,
-        pd.Series([w, x, y, z, z, x, y, w, y, y],
+        pd.Series([w, x, y, z, x, y, w, y, y],
                   index=alternatives.index))
     pdt.assert_series_equal(
         submarkets_multiplier,
@@ -80,15 +98,14 @@ def test_calculate_adjustment(
 
 
 def test_supply_and_demand(
-        lcm, choosers, alternatives, alt_segmenter):
+        lcm, choosers, alternatives, alt_segmenter, filtered, wxyz):
     clip_low = 0
     clip_high = 2
     price_col = 'price_col'
 
-    w = 1
-    x = 0.5
-    y = 1.25
-    z = 3 / 2
+    w, x, y, z = wxyz
+
+    filtered_choosers, filtered_alts = filtered
 
     new_price, submarkets_multiplier = supdem.supply_and_demand(
         lcm, choosers, alternatives, alt_segmenter, price_col,
@@ -97,23 +114,22 @@ def test_supply_and_demand(
     pdt.assert_series_equal(
         new_price,
         pd.Series(
-            [w, x, y, z, z, x, y, w, y, y],
-            index=alternatives.index) ** 5)
+            [w, x, y, z, x, y, w, y, y],
+            index=filtered_alts.index) ** 5)
     pdt.assert_series_equal(
         submarkets_multiplier,
         pd.Series([w, x, y, z], index=['w', 'x', 'y', 'z']))
 
 
 def test_supply_and_demand_base_ratio(
-        lcm, choosers, alternatives, alt_segmenter):
+        lcm, choosers, alternatives, alt_segmenter, filtered, wxyz):
     clip_low = 0
     clip_high = 2
     price_col = 'price_col'
 
-    w = 1
-    x = 0.5
-    y = 1.25
-    z = 3 / 2
+    w, x, y, z = wxyz
+
+    filtered_choosers, filtered_alts = filtered
 
     base_multiplier = pd.Series([w, x, y, z], index=['w', 'x', 'y', 'z'])
 
@@ -124,8 +140,8 @@ def test_supply_and_demand_base_ratio(
     pdt.assert_series_equal(
         new_price,
         pd.Series(
-            [w, x, y, z, z, x, y, w, y, y],
-            index=alternatives.index) ** 6)
+            [w, x, y, z, x, y, w, y, y],
+            index=filtered_alts.index) ** 6)
     pdt.assert_series_equal(
         submarkets_multiplier,
         pd.Series([w, x, y, z], index=['w', 'x', 'y', 'z']) ** 2)
