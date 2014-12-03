04a8c1a5796358e66100b19d643be0f0282195c2
Matt Davis <jiffyclub@gmail.com>
2014-12-03 12:30:00 -0800

    set version 1.3 for release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='urbansim',
-    version='1.2dev',
+    version='1.3',
     description='Tool for modeling metropolitan real estate markets',
     long_description=long_description,
     author='Synthicity',
