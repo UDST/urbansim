058a930f31cd9c9c4d16c1875578f3c66086cc3f
Paul Waddell <paul.waddell@gmail.com>
2016-03-08 17:36:02 -0800

    Updated copyright and docs to reflect UrbanSim Inc.

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'urbansim'
-copyright = u'2015, Autodesk'
+copyright = u'2016, UrbanSim Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -208,7 +208,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'urbansim.tex', u'urbansim Documentation',
-   u'Autodesk', 'manual'),
+   u'UrbanSim Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -238,7 +238,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'urbansim', u'urbansim Documentation',
-     [u'Autodesk'], 1)
+     [u'UrbanSim Inc.'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -252,7 +252,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'urbansim', u'urbansim Documentation',
-   u'Autodesk', 'urbansim', 'One line description of project.',
+   u'UrbanSim Inc', 'urbansim', 'One line description of project.',
    'Miscellaneous'),
 ]
 
