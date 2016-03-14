3cf9ec82fca29b91a5b35ffc89a9a3cf071c2cbc
juanshishido <juanshishido@cal.berkeley.edu>
2016-03-14 02:37:25 -0700

    use `pd.Int64Index()`
    * return pd.DataFrame with pd.Int64Index() index in `remove_rows()`
    * add `assert_empty_int64index() to use with `test_tgrtransition_with_accounting()`

--- urbansim/models/transition.py
+++ urbansim/models/transition.py
@@ -100,7 +100,9 @@ def remove_rows(data, nrows, accounting_column=None):
     remove_index = remove_rows.index
 
     logger.debug('finish: removed {} rows in transition model'.format(nrows))
-    return data.loc[data.index.diff(remove_index)], remove_index
+    data = data.loc[data.index.diff(remove_index)]
+    data = pd.DataFrame(data, index=pd.Int64Index(data.index))
+    return data, remove_index
 
 
 def add_or_remove_rows(data, nrows, starting_index=None, accounting_column=None):
