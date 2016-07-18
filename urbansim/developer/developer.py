bde889e309ffa7f83b19192a5c83cbc1c9a1d091
Ubuntu <fscottfoti@gmail.com>
2016-07-18 19:00:04 +0000

    a few small syntax errors

--- urbansim/developer/developer.py
+++ urbansim/developer/developer.py
@@ -99,7 +99,7 @@ class Developer(object):
     def pick(self, form, target_units, parcel_size, ave_unit_size,
              current_units, max_parcel_size=200000, min_unit_size=400,
              drop_after_build=True, residential=True, bldg_sqft_per_job=400.0,
-             profit_to_prob_func):
+             profit_to_prob_func=None):
         """
         Choose the buildings from the list that are feasible to build in
         order to match the specified demand.
@@ -198,7 +198,7 @@ class Developer(object):
             p = profit_to_prob_func(df)
         else:
             df['max_profit_per_size'] = df.max_profit / df.parcel_size
-            p=(df.max_profit_per_size.values / df.max_profit_per_size.sum()))
+            p=df.max_profit_per_size.values / df.max_profit_per_size.sum()
 
         if df.net_units.sum() < target_units:
             print "WARNING THERE WERE NOT ENOUGH PROFITABLE UNITS TO " \
@@ -211,7 +211,7 @@ class Developer(object):
             # If all developments have net_units of 1 than we need target_units of them.
             # So we choose the smaller of available developments and target_units.
             choices = np.random.choice(df.index.values, size=min(len(df.index), target_units),
-                                       replace=False, p)
+                                       replace=False, p=p)
             tot_units = df.net_units.loc[choices].values.cumsum()
             ind = int(np.searchsorted(tot_units, target_units, side="left")) + 1
             build_idx = choices[:ind]
